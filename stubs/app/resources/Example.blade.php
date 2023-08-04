adb36e059edb4ec652af5ea0e4d74bf6edb2149e
amin <amikavousi@gmail.com>
2023-08-04 11:15:58 +0330

    make it pretty

--- stubs/app/resources/Example.blade.php
+++ stubs/app/resources/Example.blade.php
@@ -1,2 +1,47 @@
-<h1>Welcome To Example App , Build Something Amazing</h1>
-<h2>Create by: Amin Kavousi</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example App</title>
+    <style>
+        body {
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            font-size: 16px;
+            line-height: 1.6;
+            color: #333;
+            padding: 20px;
+        }
+        h1, h2, h3 {
+            margin-top: 0;
+            margin-bottom: 20px;
+            color: #333;
+        }
+        h1 {
+            font-size: 36px;
+        }
+        h2 {
+            font-size: 30px;
+        }
+        p {
+            margin-top: 0;
+            margin-bottom: 20px;
+        }
+        a {
+            color: #1EAEDB;
+            text-decoration: none;
+        }
+        a:hover {
+            color: #0FA0CE;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<h1>Welcome To Example App, Build Something Amazing</h1>
+<h2>Created by: Amin Kavousi</h2>
+<h3>If you find this package useful, please consider giving us a star on GitHub to show your support.</h3>
+<h4>
+    Got a problem with the package? No worries! Just shoot me an email at <a href= "mailto: Amikavousi@gmail.com"> Amikavousi@gmail.com </a>, and I'll be your digital superhero.
+    or you can open an issue in : <a target="_blank" href="https://github.com/amikavousi/AppMaker/issues" > GitHub</a>
+</h4>
+</body>
+</html>
